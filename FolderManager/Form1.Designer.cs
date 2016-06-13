1ee2125db5a94f039b7d81d5da30d98f401dca03
Neo <neo@matrix.com>
2016-06-13 10:49:00 -0400

    Initial Commit for FolderManager Project

---
+++ FolderManager/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace FolderManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.foldersGrid = new System.Windows.Forms.DataGridView();
+            this.addNewPathButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.pathTextbox = new System.Windows.Forms.TextBox();
+            this.Path = new System.Windows.Forms.Label();
+            this.openFoldersButton = new System.Windows.Forms.Button();
+            this.deleteSelectedButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.filterTextBox = new System.Windows.Forms.TextBox();
+            this.filterButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.foldersGrid)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // foldersGrid
+            // 
+            this.foldersGrid.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.foldersGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.foldersGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.foldersGrid.Location = new System.Drawing.Point(3, 92);
+            this.foldersGrid.Name = "foldersGrid";
+            this.foldersGrid.RowTemplate.Height = 28;
+            this.foldersGrid.Size = new System.Drawing.Size(1292, 432);
+            this.foldersGrid.TabIndex = 0;
+            // 
+            // addNewPathButton
+            // 
+            this.addNewPathButton.Location = new System.Drawing.Point(134, 141);
+            this.addNewPathButton.Name = "addNewPathButton";
+            this.addNewPathButton.Size = new System.Drawing.Size(75, 35);
+            this.addNewPathButton.TabIndex = 1;
+            this.addNewPathButton.Text = "ADD";
+            this.addNewPathButton.UseVisualStyleBackColor = true;
+            this.addNewPathButton.Click += new System.EventHandler(this.addNewPathButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(145, 58);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(531, 26);
+            this.nameTextBox.TabIndex = 3;
+            // 
+            // pathTextbox
+            // 
+            this.pathTextbox.Location = new System.Drawing.Point(145, 93);
+            this.pathTextbox.Name = "pathTextbox";
+            this.pathTextbox.Size = new System.Drawing.Size(531, 26);
+            this.pathTextbox.TabIndex = 5;
+            // 
+            // Path
+            // 
+            this.Path.AutoSize = true;
+            this.Path.Location = new System.Drawing.Point(86, 93);
+            this.Path.Name = "Path";
+            this.Path.Size = new System.Drawing.Size(42, 20);
+            this.Path.TabIndex = 4;
+            this.Path.Text = "Path";
+            // 
+            // openFoldersButton
+            // 
+            this.openFoldersButton.Location = new System.Drawing.Point(235, 141);
+            this.openFoldersButton.Name = "openFoldersButton";
+            this.openFoldersButton.Size = new System.Drawing.Size(197, 35);
+            this.openFoldersButton.TabIndex = 6;
+            this.openFoldersButton.Text = "Open Selected Folders";
+            this.openFoldersButton.UseVisualStyleBackColor = true;
+            this.openFoldersButton.Click += new System.EventHandler(this.openFoldersButton_Click);
+            // 
+            // deleteSelectedButton
+            // 
+            this.deleteSelectedButton.Location = new System.Drawing.Point(455, 141);
+            this.deleteSelectedButton.Name = "deleteSelectedButton";
+            this.deleteSelectedButton.Size = new System.Drawing.Size(171, 37);
+            this.deleteSelectedButton.TabIndex = 7;
+            this.deleteSelectedButton.Text = "Delete All Selected";
+            this.deleteSelectedButton.UseVisualStyleBackColor = true;
+            this.deleteSelectedButton.Click += new System.EventHandler(this.deleteSelectedButton_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.foldersGrid, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.63321F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.44922F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 36.91756F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1298, 837);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.addNewPathButton);
+            this.groupBox1.Controls.Add(this.deleteSelectedButton);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.openFoldersButton);
+            this.groupBox1.Controls.Add(this.Path);
+            this.groupBox1.Controls.Add(this.nameTextBox);
+            this.groupBox1.Controls.Add(this.pathTextbox);
+            this.groupBox1.Location = new System.Drawing.Point(3, 530);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1091, 267);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Actions";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.resetButton);
+            this.groupBox2.Controls.Add(this.filterButton);
+            this.groupBox2.Controls.Add(this.filterTextBox);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(3, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1283, 83);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Search";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Filter by";
+            // 
+            // filterTextBox
+            // 
+            this.filterTextBox.Location = new System.Drawing.Point(145, 29);
+            this.filterTextBox.Name = "filterTextBox";
+            this.filterTextBox.Size = new System.Drawing.Size(261, 26);
+            this.filterTextBox.TabIndex = 1;
+            // 
+            // filterButton
+            // 
+            this.filterButton.Location = new System.Drawing.Point(441, 25);
+            this.filterButton.Name = "filterButton";
+            this.filterButton.Size = new System.Drawing.Size(75, 30);
+            this.filterButton.TabIndex = 2;
+            this.filterButton.Text = "Filter";
+            this.filterButton.UseVisualStyleBackColor = true;
+            this.filterButton.Click += new System.EventHandler(this.filterButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(533, 25);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 31);
+            this.resetButton.TabIndex = 3;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1298, 837);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Folder Manager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.foldersGrid)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView foldersGrid;
+        private System.Windows.Forms.Button addNewPathButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox pathTextbox;
+        private System.Windows.Forms.Label Path;
+        private System.Windows.Forms.Button openFoldersButton;
+        private System.Windows.Forms.Button deleteSelectedButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button filterButton;
+        private System.Windows.Forms.TextBox filterTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ FolderManager/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace FolderManager
+{
+    public partial class Form1 : Form
+    {
+        BindingList<FolderInfo> info = new BindingList<FolderInfo>();
+        XmlSerializer serializer = new XmlSerializer(typeof(List<FolderInfo>));
+        string fileName = "folders.xml";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            populateData();
+            
+            foldersGrid.DataSource = info;
+
+            SetColumnSettings();
+        }
+
+        private void SetColumnSettings()
+        {
+            if (info.Count > 0)
+            {
+                foldersGrid.Columns[0].MinimumWidth = 350;
+                foldersGrid.Columns[0].ReadOnly = true;
+                foldersGrid.Columns[1].MinimumWidth = 350;
+            }
+        }
+
+        private void populateData()
+        {
+            if (File.Exists(fileName))
+            {
+                var xmlReader = System.Xml.XmlReader.Create(fileName);
+                List<FolderInfo> retrievedList = (List<FolderInfo>)serializer.Deserialize(xmlReader);
+                info = new BindingList<FolderInfo>(retrievedList);
+                xmlReader.Close();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {                
+                var xmlwriter = System.Xml.XmlWriter.Create(fileName);                
+                serializer.Serialize(xmlwriter, info.ToList<FolderInfo>());
+                xmlwriter.Close();
+            }
+            catch { }
+        }
+
+        private void addNewPathButton_Click(object sender, EventArgs e)
+        {
+            int count = info.Count(n => n.Name.ToLower() == nameTextBox.Text.ToLower());
+            if (Directory.Exists(pathTextbox.Text) && count == 0 )
+            {
+                FolderInfo fInfo = new FolderInfo { Name = nameTextBox.Text, Link = pathTextbox.Text };
+                info.Add(fInfo);
+                RefreshGrid();
+                nameTextBox.Text = string.Empty;
+                pathTextbox.Text = string.Empty;
+            }
+            else if (count > 0)
+            {
+                MessageBox.Show("Name already exists");
+            }
+            else
+            {
+                MessageBox.Show("Path is not valid");
+            }
+        }
+
+        private void RefreshGrid()
+        {
+            filterTextBox.Text = "";
+            foldersGrid.DataSource = info;
+            SetColumnSettings();
+        }
+
+        private void openFoldersButton_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in foldersGrid.SelectedRows)
+            {
+                Process.Start(row.Cells["Link"].Value.ToString());
+            }
+        }
+
+        private void deleteSelectedButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure?", "Delete Confirmation", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                foreach (DataGridViewRow row in foldersGrid.SelectedRows)
+                {
+                    info.Remove(info.Single(n => n.Name == row.Cells["Name"].Value.ToString()));                    
+                }
+
+                RefreshGrid();
+            }
+        }
+
+        private void filterButton_Click(object sender, EventArgs e)
+        {
+            foldersGrid.DataSource = info.Where(n => n.Name.ToLower().Contains(filterTextBox.Text.ToLower())).ToList<FolderInfo>();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            foldersGrid.DataSource = info;
+        }
+
+    }
+
+    [Serializable]
+    public class FolderInfo
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+    }
+
+}
